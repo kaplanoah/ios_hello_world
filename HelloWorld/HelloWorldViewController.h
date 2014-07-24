78365ddf70dafbcfeb4b5ba83c2c02c92d3270c2
Noah Kaplan <noahkaplan4@gmail.com>
2014-07-24 02:16:14 -0700

    added view controller

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Noah Kaplan on 7/24/14.
+//  Copyright (c) 2014 Noah Kaplan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
---
+++ HelloWorld/HelloWorldViewController.m
@@ -0,0 +1,38 @@
+//
+//  HelloWorldViewController.m
+//  HelloWorld
+//
+//  Created by Noah Kaplan on 7/24/14.
+//  Copyright (c) 2014 Noah Kaplan. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
